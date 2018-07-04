e38e2c48510f026b759068d3e06681fd73350d43
Tincho04 <mach_490@hotmail.com>
2018-07-04 16:30:16 -0300

    Resolución ej 7

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,53 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+var numero1;
+var numero2;
+var suma;
+numero1 = document.getElementById("numeroUno").value
+numero2 = document.getElementById("numeroDos").value
+numero1 = parseInt(numero1);
+numero2 = parseInt(numero2);
+suma = numero1 + numero2
+alert ("la suma es: " + suma);
 }
 
 function restar()
 {
-	
+    var numero1;
+    var numero2;
+    var resta;
+    numero1 = document.getElementById("numeroUno").value
+    numero2 = document.getElementById("numeroDos").value
+    numero1 = parseInt(numero1);
+    numero2 = parseInt(numero2);
+    resta = numero1 - numero2
+    alert ("la resta es: " + resta);
 }
 
 function multiplicar()
 { 
-	
+    var numero1;
+    var numero2;
+    var multipli;
+    numero1 = document.getElementById("numeroUno").value
+    numero2 = document.getElementById("numeroDos").value
+    numero1 = parseInt(numero1);
+    numero2 = parseInt(numero2);
+    multipli = numero1 * numero2
+    alert ("al multiplicarlos da: " + multipli);	
 }
 
 function dividir()
 {
-	
+    var numero1;
+    var numero2;
+    var divic;
+    numero1 = document.getElementById("numeroUno").value
+    numero2 = document.getElementById("numeroDos").value
+    numero1 = parseInt(numero1);
+    numero2 = parseInt(numero2);
+    divic = numero1 / numero2
+    alert ("al dividirlos da: " + divic);	
 }
 
