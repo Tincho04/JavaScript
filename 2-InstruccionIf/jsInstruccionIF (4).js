38a6aee2e3ec1975ca60883516a947627deaf5c0
Tincho04 <mach_490@hotmail.com>
2018-07-08 19:23:14 -0300

    Interrogante IF ej 4

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,14 @@
 function mostrar()
 {
 //tomo la edad  
-
-
+var edad;
+var mensaje;
+edad=document.getElementById("edad").value;
+edad=parseInt(edad);
+mensaje=edad>=13 && <=17;   // ¿Como agregar dos condiciones? 
+if(mensaje)
+{
+    alert("Usted es adolescente");
+}
 
 }//FIN DE LA FUNCIÓN
