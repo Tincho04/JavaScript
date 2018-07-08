2332b1314405f4e66466edfc419dbabb6f589301
Tincho04 <mach_490@hotmail.com>
2018-07-08 19:34:29 -0300

    IF Ej 6

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,25 @@
 function mostrar()
 {
 //tomo la edad  
-
-
-
-
+var edad;
+var adulto;
+var adolescente;
+var niño;
+edad=document.getElementById("edad").value;
+edad=parseInt(edad);
+adulto=edad>=18  
+adolescente=edad>=13 && edad<=17;
+niño=edad<13
+if(adulto)
+{
+    alert("Usted es un adulto");
+}
+if(adolescente)
+{
+    alert("Usted es adolescente");
+}
+if(niño)
+{
+    alert("Usted es un niño");
+}
 }//FIN DE LA FUNCIÓN
