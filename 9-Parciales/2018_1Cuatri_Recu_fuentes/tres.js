78508c5f3f2b0150c075cf4627b44870b468022f
Tincho04 <mach_490@hotmail.com>
2018-07-10 09:18:15 -0300

    Parcial Recu 1-3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,25 @@
 function mostrar()
 {
+var precio;
+var porcentaje;
+var descuento;
+var preciod;
+var iva;
+var preciof;
+precio=prompt("Ingrese el precio");
+porcentaje=prompt("Ingrese el porcentaje de descuento");
+precio=parseInt(precio);
+porcentaje=parseInt(porcentaje);
 
+descuento=(precio*porcentaje)/100;
+
+preciod=precio-descuento;
+
+iva=preciod*21/100;
+
+preciof=preciod+iva;
+
+alert("el descuento en dinero es de: " +descuento+", el precio con el descuento es de: "+preciod+" y el IVA es de: "+iva);
+
+document.getElementById("elPrecioFinal").value=preciof;
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,15 @@
 
 function mostrar()
 {
-
+var base;
+var altura;
+var superficie;
+var perimetro;
+base=prompt("Ingrese la Base del triangulo");
+altura=prompt("Ingrese la Altura del triangulo");
+base=parseInt(base);
+altura=parseInt(altura);
+superficie=(base*altura)/2
+perimetro=base*3
+alert("la superficie de su triangulo es: " + superficie + " y el perimetro es: " +perimetro);
 }
