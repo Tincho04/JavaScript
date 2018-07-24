6c3be5ce8d08f2f426e761296f130a18cae52187
Tincho04 <mach_490@hotmail.com>
2018-07-24 11:08:06 -0300

    while8

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -2,21 +2,18 @@ function mostrar()
 {
 
 	var contador=0;
-	var positivo;
-	var negativo;
 	var suma=0;
 	var multiplicacion=1;
-	
 	var respuesta='si';
 while(respuesta=="si"){
-	positivo=prompt("Ingrese su numero a sumar");
-	positivo=parseInt(positivo);
-	negativo=prompt("Ingrese su numero a multiplicar");
-    negativo=parseInt(negativo);
-	suma=positivo+suma;
-	multiplicacion=negativo*multiplicacion;
-	respuesta=prompt("¿desea continuar?");
-
+	acumulador=prompt("Ingrese su numero");
+	acumulador=parseInt(acumulador);
+if(acumulador>=0){
+	suma=acumulador+suma;
+}else{
+	multiplicacion=acumulador*multiplicacion;
+}
+respuesta=prompt("¿desea continuar?");
 }
 
 document.getElementById('suma').value=suma;
