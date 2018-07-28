67ca38b412ea9191e0e27b56ab3cefd82b9bb3bd
Tincho04 <mach_490@hotmail.com>
2018-07-28 17:28:53 -0300

    For 1-2

--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,13 @@
 function mostrar()
 {
+    var cont;
+    var numerod=10;
+
+    for (cont=0; cont<10; cont++) {
+        alert(numerod);
+        numerod=numerod-1
+
+    }
 
 
 }
--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,12 @@
 function mostrar()
 {
 
-var repetciones = prompt("ingrese el número de repeticiones");
+var repeticiones = prompt("ingrese el número de repeticiones");
+var cont;
+var mensaje="Hola UTN FRA";
 
-
-
+repeticiones=parseInt(repeticiones)
+for (cont=0; cont>=repeticiones; cont++)
+    {alert(mensaje);
+    }
 }//FIN DE LA FUNCIÓN
