356739d09805757c1904bbfb437ec91c7a0dd435
Tincho04 <mach_490@hotmail.com>
2018-07-29 12:05:29 -0300

    Recu 5-6

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,21 @@
 function mostrar()
 {
+var dia=prompt("Ingrese un día de la semana");
+switch(dia){
+
+    case "lunes":
+    case "martes":
+    case "miercoles":
+    case "jueves":
+    case "viernes":
+        alert("a trabajar.");
+        break;
+    case "sabado":
+    case "domingo":
+        alert("buen finde.");
+        break;
+    default:
+        alert("no es un día válido.");
+}
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/seis.js
@@ -1,4 +1,33 @@
 function mostrar()
 {
-
+var nota=document.getElementById("laHora").value;
+switch(nota){
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+        alert("la próxima se puede.");
+        break;
+    case "4":   
+    case "5":
+    case "6":
+        if(nota<5){
+            alert("raspando. Debes preocuparte mas.");
+        }else{
+            alert("raspando.");
+        }
+        break;
+    case "7":
+    case "8":
+    case "9":
+    case "10":
+        if(nota>8){
+            alert("Aprobó. Muy bien.");
+        }else{
+            alert("aprobó.");
+        }
+        break;
+    default:
+        alert("Ingrese una nota válida.");
+}
 }
