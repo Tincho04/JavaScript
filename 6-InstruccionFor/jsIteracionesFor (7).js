d4fc6489f8fe042255f382c6000d8876adaed65c
Tincho04 <mach_490@hotmail.com>
2018-07-31 12:23:50 -0300

    for7

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -2,25 +2,20 @@ function mostrar()
 {
 var numero;
 var contdiv=0;
-var divi=0, divip=0;
+var divi=0; 
+var divip=0;
 
 
 numero=prompt("Ingrese su numero");
 numero=parseInt(numero);
 
 for(let j=numero; j>1; j=j-1){
-    if(j%2==0){
+    if(numero%j==0){
         contdiv++;
-        divip=j-2;
+        divip=j
+        /*console.log(divip)*/
         document.write("<br>" + divip)
-    }else
-        contdiv++;
-        divi=j;
-        document.write("<br>" + divi);  
-
-    }
-
-
-
+}
+}
 document.write("<br>" + "La cantidad de divisores de este numero es de: " + contdiv);
 }//FIN DE LA FUNCIÓN
