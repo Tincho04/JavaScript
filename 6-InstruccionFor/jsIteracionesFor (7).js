c36dd1f416f7c2af811f204f72b18705748506a2
Tincho04 <mach_490@hotmail.com>
2018-08-01 22:47:02 -0300

    For 7-8

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -2,8 +2,7 @@ function mostrar()
 {
 var numero;
 var contdiv=0;
-var divi=0; 
-var divip=0;
+var divi=0;
 
 
 numero=prompt("Ingrese su numero");
@@ -12,9 +11,9 @@ numero=parseInt(numero);
 for(let j=numero; j>1; j=j-1){
     if(numero%j==0){
         contdiv++;
-        divip=j
+        divi=j
         /*console.log(divip)*/
-        document.write("<br>" + divip)
+        document.write("<br>" + divi)
 }
 }
 document.write("<br>" + "La cantidad de divisores de este numero es de: " + contdiv);
--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,14 +1,21 @@
 function mostrar()
 {
-var numero;
+var numero, a=0, i;
 
 numero=prompt("ingrese su numero.");
 numero=parseInt(numero);
-
-for(let i=numero; i>=0; i-1){
-    if(i%numero==0 && i%1==0){
-
+if(isNaN(numero)){
+    numero=prompt("Ingrese un numero válido.");
+    numero=parseInt(numero);
+}
+for(i=1; i<=numero; i++){
+    if(numero%i==0){
+        a++;
     }
 }
+if (a!=2){
+    alert("Este numero no es Primo");
+}else{
     alert("Este numero es primo");
+}
 }//FIN DE LA FUNCIÓN
